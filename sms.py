effc94cfd72d1ea7d4fe31c413e237268496045d
Akshay kumar <akshay1.kumar@lntinfotech.com>
2021-07-09 11:41:28 +0530

    First commit

---
+++ sms.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client 
+ 
+account_sid = 'AC24d5683df5a21610b3a2121474e3b03e' 
+auth_token = 'f097067d99b418d9a97909d8d1524c3e' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create(  
+                              messaging_service_sid='MGc46faecba598a1dcc7ab7a0b206ee79b', 
+                              body='Hey! My chubby, If I could order your steps, I’ll have brought you my way quite sooner than now. 😘 ❤',      
+                              to='+919773502080' 
+                          ) 
+ 
+print(message.sid)
+print('Message sent successfully!')
